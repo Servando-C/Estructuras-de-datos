10a74129a807887c37f75a1f96515e34ff2ab660
Servando <servando_cs23@hotmail.com>
2021-10-17 17:19:03 -0500

    1

--- p2.py
+++ p2.py
@@ -1,25 +1,145 @@
-import random
+import random #se importa modulo random
+import time #se importa modulo time
+import sys #se importa modulo sys
+
+sys.setrecursionlimit(25000) #aumento del limite de recursion
+
 def QuickSort(arr, p, r):
-    if p < r:
-        q = Particionar(arr, p, r)
-        QuickSort(arr, p, q-1)
-        QuickSort(arr, q+1, r)
+    if p < r: #se comprueba que el arreglo no este vacio
+        q = Particionar(arr, p, r) #particionar arreglo
+        QuickSort(arr, p, q-1) #volver a llamar Quicksort con una primera part del arreglo
+        QuickSort(arr, q+1, r) #volver a llamar Quicksort con la parte restante del arreglo
 
-def Particionar(arr, p, r):
-    x = arr[r]
-    i = p-1
-    for j in range(p, r):
-        if arr[j] <= x:
-            i = i+1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i+1], arr[r] = arr[r], arr[i+1]
-    return (i+1)
+def Particionar(arr, p, r):#funcion particionar
+    x = arr[r] #seleccion de un pivote
+    i = p-1 #contador
+    for j in range(p, r): #para el rango de numeros del arreglo recibido
+        if arr[j] <= x: #si elemento de j es menor que el pivote, los intercambia
+            i = i+1 #aumento del contador
+            arr[i], arr[j] = arr[j], arr[i] #intercambio
+    arr[i+1], arr[r] = arr[r], arr[i+1] #intercambio de ultimos elementos
+    return (i+1) #retorna el valor que divide al arreglo
 
-n = 2000
+def MaxHeapify(arr, i, tamHeap): #comprueba que se cumpla la estrcutura del Heap
+    idxIzq = 2*i+1
+    idxDer = 2*i+2
+
+    idxMax = i
+
+    if (idxIzq < tamHeap) and (arr[idxIzq] > arr[i]): #comprueba si el valor de la izquierda es mas grande
+        idxMax = idxIzq #intercambia valores
+
+    if (idxDer < tamHeap) and (arr[idxDer] > arr[idxMax]): #comprueba si el valor de la derecha es el mas grande
+        idxMax = idxDer #intercambia valores
+
+    if (idxMax != i): #hasta que el valor de i y idxMax sea igual
+        arr[i], arr[idxMax] = arr[idxMax], arr[i]
+        MaxHeapify(arr, idxMax, tamHeap) #comprueba que la estructura cumpla, debido a que pueden quedar elementos 
+        #que no cumplan con la estructura
+
+def construirHeapMaxIni(arr): #construye Heap de acuerdo al tamaño de arr
+    tamHeap = len(arr) # obtencion tamaño del Heap
+    for i in range((tamHeap - 1) // 2, -1, -1): # se recorre el areglo hacia atras 
+        MaxHeapify(arr, i, tamHeap) #
+
+def HeapSort(arr): #estrcutur de datos Heap Sort
+    construirHeapMaxIni(arr) #funcion para construir
+    tamHeap = len(arr) # obtencion tamaño del Heap
+    for i in range(len(arr)-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0] # se recorre el areglo hacia atras 
+        tamHeap -= 1 #disminucion tamaño del heap
+        MaxHeapify(arr, 0, tamHeap) #se asegura que el nuevo heap formado cumpla con la estrcutura
+
+def Random_Quicksort(arr, p, r): #funcion quicksort con pivote random
+    if(p < r): #se comprueba que el arreglo no este vacio
+        q = Particionar_Rand(arr, p, r) #particionar arreglo
+        Random_Quicksort(arr, p, q-1)  #volver a llamar Quicksort con una primera part del arreglo
+        Random_Quicksort(arr, q+1, r)  #volver a llamar Quicksort con la parte restante del arreglo
+
+def Particionar_Rand(arr, p, r): #particionar con pivote random
+    randpivot = random.randrange(p, r) # obtencion de un pivote random
+    arr[p], arr[randpivot] = arr[randpivot], arr[p] #intercambio de valores tal que el pivote quede en la primera posicion
+    return Particionar(arr, p, r) #retorna el valor resultado de particionar
+
+n = 5000 #numero de elementos del arreglo
 a = []
-for i in range(n):
+for i in range(n):#se llena el arreglo con valores entre 0 y 10
     a.append(random.randint(0,10))
 
-print(a)
-QuickSort(a, 0, len(a)-1)
-print(a)
+arr = a[:] #copias de los arreglos
+arr1 = a[:]
+arr2 = a[:]
+
+print("Peor caso\n")
+
+arr.sort(reverse=True)
+arr1_1 = arr[:]
+arr1_2 = arr[:]
+arr1_3 = arr[:]
+
+t1 = time.time() # toma de tiempo
+QuickSort(arr1_1,0, len(arr)-1) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nQuickSort: t = ",tf,"s") # impresion de los resultados
+
+t1 = time.time() # toma de tiempo
+Random_Quicksort(arr1_2,0, len(arr)-1) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nRandom Quicksort: t = ",tf,"s") # impresion de los resultados
+
+t1 = time.time() # toma de tiempo
+HeapSort(arr1_3) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nHeap Sort: t = ",tf,"s") # impresion de los resultados
+
+print("\nCaso promedio")
+
+arr2_1 = arr1[:]
+arr2_2 = arr1[:]
+arr2_3 = arr1[:]
+
+t1 = time.time() # toma de tiempo
+QuickSort(arr2_1, 0, len(arr1)-1) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nQuickSort: t = ",tf,"s") # impresion de los resultados
+
+t1 = time.time() # toma de tiempo
+Random_Quicksort(arr2_2, 0, len(arr1)-1) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nRandom Quicksort: t = ",tf,"s") # impresion de los resultados
+
+t1 = time.time() # toma de tiempo
+HeapSort(arr2_3) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nHeap Sort: t = ",tf,"s") # impresion de los resultados
+
+print("\nMejor caso")
+
+arr2.sort()
+arr3_1 = arr2[:]
+arr3_2 = arr2[:]
+arr3_3 = arr2[:]
+
+t1 = time.time() # toma de tiempo
+QuickSort(arr3_1, 0, len(arr2)-1) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nQuickSort: t = ",tf,"s") # impresion de los resultados
+
+t1 = time.time() # toma de tiempo
+Random_Quicksort(arr3_2, 0, len(arr2)-1) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nRandom Quicksort: t = ",tf,"s") # impresion de los resultados
+
+t1 = time.time() # toma de tiempo
+HeapSort(arr3_3) # funcion a tomar tiempo
+t2 = time.time() # toma de tiempo
+tf = t2 - t1 # obtencion del tiempo requerido para la funcion
+print("\nHeap Sort: t = ",tf,"s") # impresion de los resultados
--- p4.py
+++ p4.py
@@ -9,39 +9,39 @@ def busquedaLinealM(arr, llave): # buesqueda lineal
     return e #en caso de no encontar la llave se retorna -1
             
 def busquedaLinealCentinela(arr, llave): #busqueda Lineal con Centinela
-    tmp = arr[len(arr)-1]
-    arr[len(arr)-1] = llave
-    k = 0
+    tmp = arr[len(arr)-1] #ultimo elemento como temporal
+    arr[len(arr)-1] = llave #sustituir ultimo elemento como la llave
+    k = 0 #contador
 
-    while (arr[k] != llave):
-        k += 1
-    arr[len(arr)-1] = tmp
+    while (arr[k] != llave): #se comprueba que el elemento del arrelgo no sea igual a la llave
+        k += 1 #indice en aumento
+    arr[len(arr)-1] = tmp #encontrada la llave el ultimo elemento recupera su valor
 
-    encontrado = None
-    if (k < len(arr)-1 or arr[len(arr)-1] == llave):
-        encontrado = k
-    return encontrado
+    encontrado = None #encontrado como none en caso de no encontrarlo
+    if (k < len(arr)-1 or arr[len(arr)-1] == llave):#condiciones para comprobar que se esta dentro del arreglo
+        encontrado = k #iguala valor al encontrado
+    return encontrado #retorna resultado
 
-def busquedaBinaria(arr, llave, ini, fin):
-    if ini > fin:
+def busquedaBinaria(arr, llave, ini, fin):#busqueda binaria
+    if ini > fin:#arreglo no vacio
         return None
 
-    mitad = (ini + fin) // 2
-    if(llave == arr[mitad]):
-        return mitad
-    elif llave < arr[mitad]:
-        return busquedaBinaria(arr, llave, ini, mitad-1)
-    else:
-        return busquedaBinaria(arr, llave, mitad+1, fin)
+    mitad = (ini + fin) // 2 #obtiene numero entero para la dividir el arreglo
+    if(llave == arr[mitad]): #si la llave esta en la division, retorna mitad
+        return mitad 
+    elif llave < arr[mitad]:#si la llave esta en la primera mitad, vuelve a llamar a si misma
+        return busquedaBinaria(arr, llave, ini, mitad-1)#recursividad con indicees de la primera mitad
+    else: #si esta en la segunda mitad, se vuelve a llamar a si misma
+        return busquedaBinaria(arr, llave, mitad+1, fin)#recursividad con indicees de la segunda mitad
 
 def BusquedaVecinos(arr, x, r, m):#funcion buscar m elementos en un radio r
-    l = []
-    i = 0
-    while len(l) <= m and i <= len(arr)-1:
-        if (abs(x-arr[i])) <= r:
-            l.append(arr[i])
-        i += 1
-    return l
+    l = [] #lista para guardar valores
+    i = 0 # se usara como indice
+    while len(l) <= m and i <= len(arr)-1: #mientras la lista no este llena y se permanezca dentro del indice
+        if (abs(x-arr[i])) <= r: #se compara la distancia entre numros con el radio
+            l.append(arr[i]) #se agrega el dato a la lista
+        i += 1 #aumento el indice
+    return l #retorna la lista
 
 n = 1000000 #tamaño del arreglo
 a = [] #arreglo
@@ -71,12 +71,12 @@ t1 = perf_counter() #toma de tiempo
 busBin = busquedaBinaria(a3, llave, a3[0], a3[len(a3)-1])
 t2 = perf_counter() #toma de tiempo
 tf = t2 - t1 #calculo tiempo final
-print("\nBusqueda Binaria: n = ",n,", llave = ",llave," y t = ",tf," s") # impresion de los resultados
-"""
+print("\nBusqueda Binaria: n = ",n,", llave = ",llave," y t = ",tf," s\n") # impresion de los resultados
+
 x = 11 #numeros a buscar cercanos
 r = 5 #radio de distancia de elementos a buscar
 m = 10 #numero de elementos a buscar
 
 en = BusquedaVecinos(a, x, r, m)
-for i in range(len(en)):
-    print(en[i],"(r = distancia a",x,"=|",en[i],"-",x,"|=",abs(en[i]-x),")")"""
+for i in range(len(en)):#imprime los datos 
+    print(en[i],"(r = distancia a",x,"=|",en[i],"-",x,"|=",abs(en[i]-x),")")
