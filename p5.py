4ffa45a18016fa302495a9f0d5cfe30c10162b5a
Servando <servando_cs23@hotmail.com>
2021-10-21 16:27:22 -0500

    1

--- p5.py
+++ p5.py
@@ -8,59 +8,85 @@ class Nodo:
     def verNodo(self):
         return self.info
 
-class linkList(object):
+class linkList:
     def __init__(self):
         self.prim = None
     
     def insertarNodo(self, dato):
         temp = Nodo(dato)
-        temp.siguiente = self.prim
-        self.prim = temp
-    
+        sp = self.prim
+        if (sp == None):
+            temp.siguiente = self.prim
+            sp = temp
+        elif(sp.siguiente == None):
+            sp.siguiente = temp
+        else:
+            temp.siguiente = None
+
     def vacia(self):
-        if self.primero == None:
+        if self.prim == None:
             return True
         else:
             return False
+    
+    def encontrar(self, dato):
+        aux = self.prim
+        k = 0
+        if(aux.info[0] == dato):
+            return aux
+        elif(aux.siguiente.info[0] == dato):
+            aux = aux.siguiente
+            return aux
+        while aux != None:
+            aux = aux.siguiente
 
+            
 n = 100
 uBase = Usuario.GetUsuariosDB(n)
 
 def CrearTablaHash(m):
-    t = linkList*m
+    t = []
+    for i in range(m):
+        a = linkList()
+        t.append(a)
+    
     return t
 
-def CalcularHash(cadena, m, i):
+def CalcularHash(cadena, m):
     sum = 0
-    aux = 0
-    for i in range(len(cadena)):
-        sum += (i+1)*ord(cadena[i])
-
-    hash = round((sum + i) % 11 + (aux//10)) #hash por metodo de division
+    for j in range(len(cadena)):
+        sum += (j+1)*ord(cadena[j])
+    
+    p = round(1.7*m) #asignacion de valores aleatorios para calcular hash
+    a = 0.9*p
+    b = 0.2*p
+    k = sum+i
+    hash = round(((a*k+b) % p) % m) #hash por metodo universal
     return hash
 
 def Agregar(t, key, valor):
     m = len(key)
     i = 0 # sondeo lineal
     while i < m: #calcular hash de la llave
-        hash = CalcularHash(key, m, i)
+        hash = CalcularHash(key, m)
         par = [key, valor]
-        if t[hash] == []:
-            t[hash] = par
-            return hash
-        else:
-            i += 1
+        #if t[hash].vacia():
+        t[hash].insertarNodo(par)
+        return hash
+        """else:
+            t[hash].
+            i += 1"""
     print("Tabla llena")
 
 def Buscar(t, key):
     m = len(t)
     i = 0 
-    while i< m:
-        if t[hash] == key:
+    hash = CalcularHash(key, len(key))
+    while i < m:
+        if t[hash].prim.info[0] == key:
             return CalcularHash
-        elif t[hash] == None:
-            return None
         else:
+            t[hash].encontrar(key)
             i += 1
     return None
 
--- p6.py
+++ p6.py
@@ -1,11 +1,25 @@
 class Nodo():
 
-	def __init__(self, nombre):
-		self.nombre = nombre
+	def __init__(self, nombre):#clase nodo
+		self.nombre = nombre #atributos del nodo
 		self.vecinos = []
+		self.color = "blanco"
+		self.distancia = -1
+		self.padre = None 
 
 	def agregarVecino(self, nodo):
+		for v in self.vecinos:
+			if v.nombre == nodo.nombre:
+				print("Nodo existente")
+				return
 		self.vecinos.append(nodo)
+
+	def __str__(self):
+		return self.nombre
+
+	def __repr__(self) -> str:
+		return self.nombre
+
 
 class Grafo():
 
@@ -33,7 +47,53 @@ class Grafo():
 		nodo1.agregarVecino(nodo2)
 		nodo2.agregarVecino(nodo1)
 
+	def bfs(self, nodoIni):
+		for u in self.vertices.values():
+			u.color = "blanco"
+			u.distancia = -1
+			u.padre = None
+
+		self.vertices[nodoIni].color = "gris"
+		self.vertices[nodoIni].distancia = 0
+		self.vertices[nodoIni].padre = None
+
+		q = [] #encolar
+		q.append(nodoIni) #desencolar
+		while len(q) > 0:
+			u = q.pop()
+			for v in u.vecinos:
+				if v.color == "blanco":
+					v.color = "gris"
+					v.distancia = u.distancia + 1
+					v.padre = u
+					cola.append(v)
+			u.color = "negro"
+	
+	def __str__(self):
+		s = ""
+		for v in self.vertices:
+			s += self.vertices[v].nombre + "-"
+			for i in self.vertices[v].vecinos:
+				s =+ i.nombre + ","
+			s += "\n"
+
+	def __repr__(self):
+		for v in self.vertices:
+			s += self.vertices[v].nombre + ", "
+		return s
 
 g = Grafo()
 g.agregarVertice('1')
 g.agregarVertice('2')
+g.agregarVertice('3')
+g.agregarVertice('4')
+g.agregarVertice('5')
+g.agregarVertice('6')
+
+g.agregarArista('1','1')
+g.agregarArista('1','2')
+g.agregarArista('1','5')
+
+g.bsc("1")
+
+
