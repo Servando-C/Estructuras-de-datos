fc1719aa0a44bd6fbf153f7e2bce023b2dc2570e
Servando <servando_cs23@hotmail.com>
2021-10-21 19:17:03 -0500

    1

--- p6.py
+++ p6.py
@@ -30,19 +30,22 @@ class Grafo():
 		nuevoNodo = Nodo(nombreNodo)
 		self.vertices[nombreNodo] = nuevoNodo
 
-	def agregarArista(self, nodo1, nodo2):
+	def agregarArista(self, nombreNodo1, nombreNodo2):
 
-		if nodo1 in self.vertices:
-			n1 = self.vertices[nodo1]
+		if nombreNodo1 in self.vertices:
+			nodo1 = self.vertices[nombreNodo1]
 		else:
-			print("No existe nodo con nombre ",+nodo1)
+			print("No existe nodo con nombre ",+nombreNodo1)
 			return
 
-		if nodo2 in self.vertices:
-			n2 = self.vertices[nodo2]
+		if nombreNodo2 in self.vertices:
+			nodo2 = self.vertices[nombreNodo2]
 		else:
-			print("No existe nodo con nombre ",+nodo1)
+			print("No existe nodo con nombre ",+nombreNodo2)
 			return
+
+		nodo1 = self.vertices[nombreNodo1]
+		nodo2 = self.vertices[nombreNodo2]
 
 		nodo1.agregarVecino(nodo2)
 		nodo2.agregarVecino(nodo1)
@@ -53,20 +56,22 @@ class Grafo():
 			u.distancia = -1
 			u.padre = None
 
-		self.vertices[nodoIni].color = "gris"
-		self.vertices[nodoIni].distancia = 0
-		self.vertices[nodoIni].padre = None
+		self.color = "gris"
+		self.distancia = 0
+		self.padre = None
 
 		q = [] #encolar
 		q.append(nodoIni) #desencolar
+		nodo_u = self.vertices[u]
+
 		while len(q) > 0:
-			u = q.pop()
-			for v in u.vecinos:
+			u = q.pop(0)
+			for v in nodo_u.vecinos:
 				if v.color == "blanco":
 					v.color = "gris"
 					v.distancia = u.distancia + 1
 					v.padre = u
-					cola.append(v)
+					q.append(v) #tenia cola
 			u.color = "negro"
 	
 	def __str__(self):
@@ -78,6 +83,7 @@ class Grafo():
 			s += "\n"
 
 	def __repr__(self):
+		s = ""
 		for v in self.vertices:
 			s += self.vertices[v].nombre + ", "
 		return s
@@ -94,6 +100,7 @@ g.agregarArista('1','1')
 g.agregarArista('1','2')
 g.agregarArista('1','5')
 
-g.bsc("1")
+g.bfs(g.vertices["1"])
 
+print()
 
