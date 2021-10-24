8a71ebb799dc681ddc4f8b995496c610fc297d69
Servando <servando_cs23@hotmail.com>
2021-10-23 22:57:30 -0500

    1

--- p6.py
+++ p6.py
@@ -10,7 +10,7 @@ class Nodo():
 	def agregarVecino(self, nodo):
 		for v in self.vecinos:
 			if v.nombre == nodo.nombre:
-				print("Nodo existente")
+				print("Ya existe el vecino "+nodo.nombre+" en el nodo "+self.nombre)
 				return
 		self.vecinos.append(nodo)
 
@@ -27,6 +27,9 @@ class Grafo():
 		self.vertices = {}
 
 	def agregarVertice(self, nombreNodo):
+		for v in self.vertices:
+			if v == nombreNodo:
+				print("Ya existe el vertice "+nombreNodo)
 		nuevoNodo = Nodo(nombreNodo)
 		self.vertices[nombreNodo] = nuevoNodo
 
@@ -35,7 +38,7 @@ class Grafo():
 		if nombreNodo1 in self.vertices:
 			nodo1 = self.vertices[nombreNodo1]
 		else:
-			print("No existe nodo con nombre ",+nombreNodo1)
+			print("No existe nodo con nombre "+nombreNodo1)
 			return
 
 		if nombreNodo2 in self.vertices:
@@ -50,23 +53,24 @@ class Grafo():
 		nodo1.agregarVecino(nodo2)
 		nodo2.agregarVecino(nodo1)
 
-	def bfs(self, nodoIni):
+	def bfs(self, nombreNodo):
+
+
 		for u in self.vertices.values():
 			u.color = "blanco"
 			u.distancia = -1
 			u.padre = None
 
-		self.color = "gris"
-		self.distancia = 0
-		self.padre = None
+		self.vertices[nombreNodo].color = "gris"
+		self.vertices[nombreNodo].distancia = 0
+		self.vertices[nombreNodo].padre = None
 
 		q = [] #encolar
-		q.append(nodoIni) #desencolar
-		nodo_u = self.vertices[u]
+		q.append(self.vertices[nombreNodo]) #desencolar
 
 		while len(q) > 0:
 			u = q.pop(0)
-			for v in nodo_u.vecinos:
+			for v in u.vecinos:
 				if v.color == "blanco":
 					v.color = "gris"
 					v.distancia = u.distancia + 1
@@ -75,32 +79,48 @@ class Grafo():
 			u.color = "negro"
 	
 	def __str__(self):
-		s = ""
+		s = ''
 		for v in self.vertices:
-			s += self.vertices[v].nombre + "-"
+			s += self.vertices[v].nombre + ' -> ['
+			j = 0
 			for i in self.vertices[v].vecinos:
-				s =+ i.nombre + ","
-			s += "\n"
+				j += 1
+				if len(self.vertices[v].vecinos) == j:
+					s += ""+i.nombre
+				else:
+					s += ""+i.nombre + ','
+			s += "]\n"
+		return s
 
 	def __repr__(self):
-		s = ""
+		s = ''
 		for v in self.vertices:
 			s += self.vertices[v].nombre + ", "
 		return s
 
 g = Grafo()
+g.agregarVertice('0')
 g.agregarVertice('1')
 g.agregarVertice('2')
 g.agregarVertice('3')
 g.agregarVertice('4')
 g.agregarVertice('5')
 g.agregarVertice('6')
+g.agregarVertice('7')
 
-g.agregarArista('1','1')
 g.agregarArista('1','2')
-g.agregarArista('1','5')
+g.agregarArista('1','0')
+g.agregarArista('1','0')
+g.agregarArista('2','0')
+g.agregarArista('2','3')
+g.agregarArista('0','3')
+g.agregarArista('3','4')
+g.agregarArista('5','4')
+g.agregarArista('5','6')
+g.agregarArista('6','4')
+g.agregarArista('9','4')
 
-g.bfs(g.vertices["1"])
+g.bfs('0')
 
-print()
+print(g)
 
