b72286d1ff6cc19588b56ddb79e0798a0e823e9f
Servando <servando_cs23@hotmail.com>
2021-11-13 22:16:01 -0600

    1

--- p8.py
+++ p8.py
@@ -1,47 +1,181 @@
 class Nodo:
-	def __init__(slef, val):
+	def __init__(self, val):
 		self.valor = val
 		self.izq = None
 		self.der = None
+		self.padre = None
+
+	def setPadre(self, nodo):
+		self.padre = nodo
 
 class abb:#arbol binario de busqueda
 	def __init__(self):
 		self.raiz = None
+	
+	def setPadre(self, nodo):
+		self.padre = nodo
+	
+	def getRaiz(self):
+		return self.raiz
 
-def agregar(self):
-	pass
+	def insertar(self, k):
+		if self.raiz == None:
+			self.raiz = Nodo(k)
+		else:
+			self.agregar(k, self.raiz)
 
-def agregar(self, k, nodo):
-	if self.raiz == None:
-		self.raiz = Nodo(k)
-	else:
-		if k < nodo.valor:
-			if nodo.izq == None:
-				nodo.izq = Nodo(k)
+	def agregar(self, k, nodo ):
+			if k < nodo.valor:
+				if nodo.izq == None:
+					nodo.izq = Nodo(k)
+					nodo.izq.setPadre(nodo)
+				else:
+					self.agregar(k, nodo.izq)
+			if k > nodo.valor:
+				if nodo.der == None:
+					nodo.der = Nodo(k)
+					nodo.der.setPadre(nodo)
+				else:
+					self.agregar(k, nodo.der)
+			if k == nodo.valor:
+				print("Valor ya existente")
+
+	def impInOrden(self, nodo = "vacio"):
+		if nodo == "vacio":
+			nodo = self.raiz
+		if nodo != None:
+			self.impInOrden(nodo.izq)
+			print(nodo.valor, end = " ")
+			self.impInOrden(nodo.der)
+
+	def impPreOrden(self, nodo):
+		if nodo != None:
+			print(nodo.valor)
+			self.impPreOrden(nodo.izq)
+			self.impPreOrden(nodo.der)
+
+	def impPostOrden(self, nodo):
+		if nodo != None:
+			print(nodo.valor)
+			self.impPostOrden(nodo.izq)
+			self.impPostOrden(nodo.der)
+
+	def anchura(self):
+		cola = []
+		if self.raiz != None:
+			while self.raiz != None:
+				nodo = cola.pop(0)
+				print(nodo.valor)
+				if nodo.izq != None:
+					cola.append(nodo.izq)
+				if nodo.der != None:
+					cola.append(nodo.der)
+	
+	def buscar(self, k, nodo = "vacio"):
+		#s = "¿Existe ",k,"?"
+		if nodo == "vacio":
+			nodo = self.raiz
+
+		if nodo != None:
+			if k == nodo.valor:
+				return True
+			if k < nodo.valor:
+				return self.buscar(k, nodo.izq)
+			if k > nodo.valor:
+				return self.buscar(k, nodo.der)
+		else:
+			return False
+
+	def maximo (self, nodo = None):
+		if nodo == None:
+			nodo = self.raiz
+		if nodo.der:
+			return self.maximo(nodo.der)
+		return nodo.valor
+
+	def minimo (self, nodo = None):
+		if nodo == None:
+			nodo = self.raiz
+		if (nodo.izq):
+			return self.minimo(nodo.izq)
+		return nodo.valor
+
+	def getNodo(self, k):
+		actual = None
+		if self.raiz != None:
+			actual = self.raiz
+			while actual is not None and actual.valor is not k:
+				if k < actual.valor:
+					actual = actual.izq
+				else:
+					actual = actual.der
+		return actual
+
+	def cambiarNodo(self, nodo, hijo):
+		if hijo != None:
+			hijo.setPadre(nodo.padre)
+		if nodo.padre != None:
+			if nodo == nodo.padre.der:
+				nodo.padre.der = hijo
 			else:
-				agregar(k, nodo.izq)
-		if k > nodo.valor:
-			if nodo.der == None:
-				nodo.der = Nodo(k)
-			else:
-				self.agregar(k, nodo.der)
-		if k == nodo.valor:
-			print("Valor ya existente")
+				nodo.padre.izq = hijo
 
-def impInOrden(self, nodo):
-	if nodo != None:
-		self.impInOrden(nodo.izq)
-		print(nodo.valor)
-		self.impInOrden(nodo.der)
+	def borrar (self, k):
+		if self.buscar(k):
+			if self.raiz != None:
+				nodo = self.getNodo(k)
+				if nodo != None:
+					if nodo.der == None and nodo.izq == None:
+						self.cambiarNodo(nodo, None)
+						nodo = None
+					elif nodo.der == None and nodo.izq != None:
+						self.cambiarNodo(nodo, nodo.izq)
+					elif nodo.der != None and nodo.izq == None:
+						self.cambiarNodo(nodo, nodo.der)
+					else:
+						aux = self.maximo(nodo.izq)
+						self.borrar(aux.valor)
+						nodo.valor = aux.valor
 
-def maximo (self, nodo):
-	if (nodo.der):
-		return maximo(nodo.der)
-	return nodo.valor
-
-def limpiar (self):
-	root = Null
-
+	def limpiar (self):
+		root = None
 
 arbol = abb()
-arbol.agregar(10)
+arbol.insertar(8)
+arbol.insertar(3)
+arbol.insertar(10)
+arbol.insertar(1)
+arbol.insertar(6)
+arbol.insertar(14)
+arbol.insertar(4)
+arbol.insertar(7)
+arbol.insertar(13)
+arbol.impInOrden()
+
+arbol.insertar(14)
+arbol.insertar(1)
+
+arbol.minimo()
+arbol.maximo()
+
+arbol.buscar(4)
+arbol.buscar(8)
+arbol.buscar(13)
+arbol.buscar(2)
+arbol.buscar(15)
+
+arbol.borrar(7)
+arbol.impInOrden()
+arbol.borrar(10)
+arbol.impInOrden()
+arbol.borrar(6)
+arbol.impInOrden()
+arbol.borrar(3)
+arbol.impInOrden()
+arbol.borrar(3)
+arbol.impInOrden()
+arbol.borrar(8)
+arbol.impInOrden()
+
+arbol.insertar(100)
+arbol.impInOrden()
--- pA.py
+++ pA.py
@@ -1,138 +1,150 @@
 class Nodo:#clase que contiene los elementos de la lista
     def __init__(self, dIni):#constructor
-        self.dato = dIni
-        self.sig = None
+        self.dato = dIni #se requiere de un valor para que funcione la clase listaPalabras
+        self.sig = None #siguiente de un nodo por defecto es None
 
-    def getDato(self):
+    def getDato(self):#metodo obtiene el dato del nodo
         return self.dato
 
-    def getSiguiente(self):
+    def getSiguiente(self):#metodo obtiene el siguiente del nodo
         return self.sig
 
-    def setDato(self,nuevodato):
+    def setDato(self,nuevodato):#metodo para ingresar el dato del nodo
         self.dato = nuevodato
 
-    def setSiguiente(self, nuevosiguiente):
+    def setSiguiente(self, nuevosiguiente):#metodo para ingresar el siguiente del nodo
         self.sig = nuevosiguiente
 
 
 class ListaOrdenada:
-    def __init__(self):
+    def __init__(self):#constructor de la clase
         self.cabeza = None
 
-    def buscar(self, valor):
-        actual = self.cabeza
-        encontrado = False
-        detenerse = False
-        while actual != None and not encontrado and not detenerse:
-            if actual.getDato() == valor:
-                encontrado = True
+    def buscar(self, valor):#metodo buscar en la lista
+        actual = self.cabeza #inicializar la primera posicion para trabajar sobre esta
+        encontrado = False #bandera por si es encontrado
+        detenerse = False #bandera por si se detiene al ya no haber mas datos
+        while actual != None and not encontrado and not detenerse: #mientras el elemento no se haya encontrado y la busqueda 
+            if actual.getDato() == valor:#no se haya detenido y el elemento actual contenga algun valor, realizar busqueda
+                encontrado = True #si el dato actual es igual al buscado se marca la bandera encontrado como True
             else:
-                if actual.getDato() > valor:
-                    detenerse = True
+                if actual.getDato() > valor:#si el dato actual es mas grande que el valor buscado, detiene la busqueda, 
+                    detenerse = True#debido a que es una lista odenada
                 else:
-                    actual = actual.getSiguiente()
-        return encontrado
+                    actual = actual.getSiguiente()#actual cambia al siguiente valor
+        return encontrado #retorna el estado encontrado como True o False 
 
-    def agregar(self, valor):
-        actual = self.cabeza
-        anterior = None
-        detenerse = False
-        while actual != None and not detenerse:
-            if actual.getDato() > valor:
-                detenerse = True
+    def agregar(self, valor):#metodo para agregar valores
+        actual = self.cabeza#empezar por la cabeza de la lista
+        anterior = None #anterior del valor actual
+        detenerse = False #bandera detenerse
+        while actual != None and not detenerse:#iniciar ciclo mientras actual tenga valor y no se haya detenido
+            if actual.getDato() > valor:#si el valor actual es mas grande que el valor inicial, detenerse
+                detenerse = True#debido a que la lista esta ordenada
             else:
-                anterior = actual
+                anterior = actual#avanzar uno en conjunto
                 actual = actual.getSiguiente()
-        temp = Nodo(valor)
-        if anterior == None:
+        temp = Nodo(valor)#nodo que guardara el valor temporalmente
+        if anterior == None:#si no tiene valor detras, se agrega unicamente nodo temp apuntando a None
             temp.setSiguiente(self.cabeza)
             self.cabeza = temp
         else:
-            temp.setSiguiente(actual)
+            temp.setSiguiente(actual)#se agrega antes de actual y despues de anterior
             anterior.setSiguiente(temp)
 
-    def quitar(self):
-        actual = self.cabeza
-        a = actual.sig
-        while actual != None:
-            if(actual.sig.sig == None):
-                actual.sig = None     
-            actual = actual.getSiguiente()
+    def quitar(self):#metodo para eliminar elemento mas grande
+        actual = self.cabeza#cabeza de la lista
+        while actual != None:#mientras actual tenga valor
+            if(actual.getSiguiente().getSiguiente() == None):#cuando el siguiente del actual sea el ultimo
+                print("Valor eliminado: "+str(actual.getSiguiente().getDato()))#imprimir su valor
+                actual.setSiguiente(None)#desvincular ultimo nodo y vincularlo a None
+            actual = actual.getSiguiente()#iterar sobre la lista
 
-    def estaVacia(self):
-        return self.cabeza == None
-
-    def len(self):
-        actual = self.cabeza
-        c = 0
-        while actual != None:
-            c = c + 1
-            actual = actual.getSiguiente()
-        return c
-    def __str__(self):
-        s = ''
-        actual = self.cabeza
-        while actual != None:
-            s = s+str(actual.dato)+"\n"
-            actual = actual.getSiguiente()
-        return s
+    def len(self):#metodo para obtener longitud de la lista
+        actual = self.cabeza#Se inicia por la cabeza de la lista
+        c = 0#Contador
+        while actual != None:#Mientras el nodo actual tenga un valor
+            c = c + 1 #Aumento de contador
+            actual = actual.getSiguiente() #Avanzar al siguiente nodo
+        return c #Retornar el contador que es el número de elementos
+    
+    def __str__(self):# Método para imprimir la lista
+        s = '' #inicio de la cadena
+        actual = self.cabeza #Se inicia por la cabeza de la lista
+        if actual == None: #Si el nodo actual no tiene ningún valor es lista vacia
+            print("Lista vacia") #imprime lista vacía
+        while actual != None: # Mientras el nodo actual tenga algún valor
+            s = s+str(actual.dato)+"\n" #agrega valores a la cadena a retornar
+            actual = actual.getSiguiente() #Avanza al siguiente nodo
+        return s # Retorna la cadena con las palabras
 
 class ListaPalabras:
-    def __init__(self, cadena):
-        if cadena == None:
+    def __init__(self, cadena=None): #Constructor necesita de una palabra para inicializarse
+        if cadena == None:# Si no hay ninguna palabra imprime error
             print("Valor necesario para inicializar la lista")
         else:
-            self.cabeza = cadena
-
-    def agregar(self, cadena):
-        temp = Nodo(cadena)
-        actual = self.cabeza
-        if self.buscar(cadena) == True:
-            print("Error: palabra repetida")
-        elif cadena == '':
-            print("Error: cadena vacia")
-        elif self.cabeza == None:
-            temp.setSiguiente(self.cabeza)
+            temp = Nodo(cadena)# Se agrega el primer nodo
+            temp.setSiguiente(None)
             self.cabeza = temp
+
+
+    def agregar(self, cadena=None):#Método para agregar palabras a la lista 
+        temp = Nodo(cadena) #nodo temporal para guardar el valor
+        if cadena == None:#Si no hay ningún elemento en la cadena la cadena está vacía
+            print("Error: cadena vacia")#Mensaje de error
+        elif self.buscar(cadena):#Busca en la lista la cadena recibida, evita repeticiones
+            print("Error: palabra repetida") #Mensaje de error
         else:
-            temp.setSiguiente(actual)
+            temp.setSiguiente(self.cabeza)#Se agrega el nuevo nodo primero en la lista
+            self.cabeza = temp 
 
-    def buscar(self, valor):
-        actual = self.cabeza
-        encontrado = False
-        detenerse = False
-        while actual != None and not encontrado and not detenerse:
-            if actual.getDato() == valor:
-                encontrado = True
+    def len(self):#metodo para obtener longitud de la lista
+        actual = self.cabeza#Se inicia por la cabeza de la lista
+        c = 0#Contador
+        while actual != None:#Mientras el nodo actual tenga un valor
+            c = c + 1 #Aumento de contador
+            actual = actual.getSiguiente() #Avanzar al siguiente nodo
+        return c #Retornar el contador que es el número de elementos
+    
+    def buscar(self, valor):#metodo buscar en la lista
+        actual = self.cabeza #inicializar la primera posicion para trabajar sobre esta
+        encontrado = False #bandera por si es encontrado
+        detenerse = False #bandera por si se detiene al ya no haber mas datos
+        while actual != None and not encontrado and not detenerse: #mientras el elemento no se haya encontrado y la busqueda 
+            if actual.getDato() == valor:#no se haya detenido y el elemento actual contenga algun valor, realizar busqueda
+                encontrado = True #si el dato actual es igual al buscado se marca la bandera encontrado como True
             else:
-                if actual.getDato() > valor:
-                    detenerse = True
+                if actual.getDato() > valor:#si el dato actual es mas grande que el valor buscado, detiene la busqueda, 
+                    detenerse = True#debido a que es una lista odenada
                 else:
-                    actual = actual.getSiguiente()
-        return encontrado
+                    actual = actual.getSiguiente()#actual cambia al siguiente valor
+        return encontrado #retorna el estado encontrado como True o False 
 
-    def __str__(self):
-        s = ''
-        c = 0
-        actual = self.cabeza
-        while actual != None:
-            if actual.sig == None:
-                s = s+str(actual.getDato())
+    def pop(self):#Método para quitar el último elemento ingresado
+        print ("pop: "+str(self.cabeza.getDato()))#imprime el valor a eliminar
+        self.cabeza = self.cabeza.getSiguiente() #Cambia el nodo cabeza por el siguiente en la lista
+
+    def __str__(self):#Método para imprimir la lista
+        s = ''#inicializar cadena
+        c = 0#contador
+        actual = self.cabeza #iniciar con nodo cabeza
+        while actual != None: #Mientras el logo actual tenga un valor
+            if actual.getSiguiente() == None: #Cuando sea el último valor
+                s = s+actual.getDato()+"\n" #imprimir sin coma
+                return s# retornar la cadena
             else:
-                s = s+str(actual.dato)+", "
-                actual = actual.getSiguiente()
-            
+                s = s+actual.getDato()+", "#imprimir con coma al final
+                actual = actual.getSiguiente()#Continuar al siguiente nodo
             c += 1
-        return s
 
 
+print("--LISTA ORDENADA--\n")
+milista = ListaOrdenada()#Inicializar una lista ordenada
 
-milista = ListaOrdenada()
+print(milista)#Imprimir elementos de la lista
+print("Longitud : "+str(milista.len())+"\n")#Imprimir longitud de la lista
 
-print(milista.len())
-
-milista.agregar(5)
+milista.agregar(5)#Agregar elementos a la lista
 milista.agregar(10)
 milista.agregar(3)
 milista.agregar(1)
@@ -141,16 +153,36 @@ milista.agregar(7)
 milista.agregar(100)
 milista.agregar(2)
 
+print("Lista:")
+print(milista)#Imprimir elementos de la lista
+print("Longitud : "+str(milista.len()))#Imprimir longitud de la lista
+
+milista.quitar()#quitar elementos mas grandes de la lista
 milista.quitar()
 
-print(milista.len())
-print(milista.buscar(93))
-print(milista.buscar(100))
+print("\nLista:")
+print(milista)#Imprimir elementos de la lista
+print("Longitud : "+str(milista.len()))#Imprimir longitud de la lista
 
-print(milista)
+print("\n--LISTA CADENAS--\n")
 
-listaCadena = ListaPalabras("Hola")
-
+listaCadena = ListaPalabras("Hola")#inicializar lista con una palabra
+listaCadena.agregar("Mundo")#Agregar palabras a la lista
+listaCadena.agregar("Ingenieria")
 listaCadena.agregar("EDA")
+listaCadena.agregar("2")
+listaCadena.agregar()#pruebas de error con palabra vacía
+listaCadena.agregar("Mundo")#Prueba de error con palabra repetida
 
-print(listaCadena)
+print("\nLista: ")
+print(listaCadena)#Imprimir elementos de la lista
+
+print("¿La palabra Ingenieria esta?: "+str(listaCadena.buscar("Ingenieria")))#Comprobar si palabras existen en la lista
+print("¿La palabra UNAM esta?: "+str(listaCadena.buscar("UNAM")))
+
+listaCadena.pop()#Quitar últimos elementos agregados a la lista
+listaCadena.pop()
+listaCadena.pop()
+
+print("\nLista: ")
+print(listaCadena)#Imprimir elementos de la lista
